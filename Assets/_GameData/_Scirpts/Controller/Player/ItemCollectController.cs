550cc2c366641cfee21e12776157f7b56667c0fb
godzago <48593494+godzago@users.noreply.github.com>
2024-07-26 14:33:56 +0300

    flashlight

--- Assets/_GameData/_Scirpts/Controller/Player/ItemCollectController.cs
+++ Assets/_GameData/_Scirpts/Controller/Player/ItemCollectController.cs
@@ -1,10 +1,13 @@
 using UnityEngine;
+using UnityEngine.Experimental.GlobalIllumination;
+using UnityEngine.XR;
 
 public class ItemCollectController : MonoBehaviour
 {
     public float CollectRange = 2.0f;
     public Transform HandPosition;
     public Camera Camera;
+    public GameObject Flashlight;
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
@@ -19,13 +22,20 @@ public class ItemCollectController : MonoBehaviour
                     CollectItem(hit.collider.gameObject);
                 }
             }
+            if (Physics.Raycast(ray, out hit, CollectRange))
+            {
+                if (hit.collider.TryGetComponent(out FlashlightController flashlight))
+                {
+                    flashlight.gameObject.SetActive(false);
+                    Flashlight.SetActive(true);
+                }
+            }
         }
     }
 
     void CollectItem(GameObject item)
     {
         Debug.Log("Eþya alýndý: " + item.name);
-
         item.transform.SetParent(HandPosition); 
         item.transform.localPosition = Vector3.zero; 
         item.transform.localRotation = Quaternion.identity;
