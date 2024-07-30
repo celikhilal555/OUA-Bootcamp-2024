7f25e8b1b31d39568c74343fe714c46f42cbc70b
OzgexSr <ozgesaritemur@gmail.com>
2024-07-30 15:54:58 +0300

    added start sceen

---
+++ Assets/_GameData/_Scirpts/StartGame.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartGame : MonoBehaviour
+{
+
+    public GameObject panel;
+    
+    public void Baslatma()
+    {
+        panel.SetActive(false);
+    }
+}
